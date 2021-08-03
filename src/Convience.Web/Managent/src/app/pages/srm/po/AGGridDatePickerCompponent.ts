d1c04dd2dc6a8809db3bb4742c13b064afcc7ce7
2842142914@qq.com <2842142914@qq.com>
2021-08-03 17:13:02 +0800

    日期限定最多早交貨7天

--- src/Convience.Web/Managent/src/app/pages/srm/po/AGGridDatePickerCompponent.ts
+++ src/Convience.Web/Managent/src/app/pages/srm/po/AGGridDatePickerCompponent.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ICellEditorAngularComp } from 'ag-grid-angular/ag-grid-angular';
 import { DatePipe } from '@angular/common';
 import { SrmPoService } from '../../../business/srm/srm-po.service';
+import { differenceInCalendarDays } from 'date-fns';
 
 @Component({
   selector: 'date-editor-cell',
   template: `
-    <nz-date-picker nzSize="large" nzAutoFocus="true" [(ngModel)]="selectedDate" (ngModelChange)="onChange($event)" style nzPlaceHolder="請選擇日期"></nz-date-picker>
+    <nz-date-picker nzSize="large" nzAutoFocus="true" [(ngModel)]="selectedDate" (ngModelChange)="onChange($event)" style nzPlaceHolder="請選擇日期" [nzDisabledDate]="disabledDate"></nz-date-picker>
   `
 })
 export class AgGridDatePickerComponent implements ICellEditorAngularComp {
@@ -15,10 +16,11 @@ export class AgGridDatePickerComponent implements ICellEditorAngularComp {
   private params: any;
 
   public selectedDate: any;
-
+  disabledDate= (current: Date): boolean =>
+  // Can not select days before today and today
+  differenceInCalendarDays(current, new Date(this.params.data.DeliveryDate)) < -7;
   agInit(params: any): void {
     this.params = params;
-
   }
 
   getValue(): any {
