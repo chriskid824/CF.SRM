7d1431c8f952342bcfc432bdff89f6e15a280cf6
CFP-TEST02\Administrator <Jenny>
2021-08-12 18:27:02 +0800

    為不報錯先補上

--- src/Convience.Backend/Convience.Applications/Convience.Entity/Entity/SRM/SrmRfqH.cs
+++ src/Convience.Backend/Convience.Applications/Convience.Entity/Entity/SRM/SrmRfqH.cs
@@ -14,6 +14,11 @@ namespace Convience.Entity.Entity.SRM
     [Entity(DbContextType = typeof(SRMContext))]
     public partial class SrmRfqH
     {
+        public SrmRfqH()
+        {
+            SrmQotHs = new HashSet<SrmQotH>();
+        }
+
         public int RfqId { get; set; }
         public string RfqNum { get; set; }
         public int? Status { get; set; }
@@ -25,5 +30,6 @@ namespace Convience.Entity.Entity.SRM
         public string LastUpdateBy { get; set; }
         public DateTime? EndDate { get; set; }
         public string EndBy { get; set; }
+        public virtual ICollection<SrmQotH> SrmQotHs { get; set; }
     }
 }
