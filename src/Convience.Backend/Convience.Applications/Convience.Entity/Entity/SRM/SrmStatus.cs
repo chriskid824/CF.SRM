cf62d84b6009b40c84236485aaef71ec7ee13727
2842142914@qq.com <2842142914@qq.com>
2021-08-19 11:44:23 +0800

    Srm_Status補檔

---
+++ src/Convience.Backend/Convience.Applications/Convience.Entity/Entity/SRM/SrmStatus.cs
@@ -0,0 +1,15 @@
+﻿using Convience.EntityFrameWork.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Convience.Entity.Entity.SRM
+{
+    [Entity(DbContextType = typeof(SRMContext))]
+    public partial class SrmStatus
+    {
+        public int Status { get; set; }
+        public string StatusDesc { get; set; }
+    }
+}
