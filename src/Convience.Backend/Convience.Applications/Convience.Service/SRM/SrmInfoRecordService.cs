01b6e77b406c1a7ff3beebcf719d7f2dd62b5a96
CFP-TEST01\Administrator <Leon>
2021-08-19 11:14:25 +0800

    infoRecord

---
+++ src/Convience.Backend/Convience.Applications/Convience.Service/SRM/SrmInfoRecordService.cs
@@ -0,0 +1,35 @@
+﻿using Convience.Entity.Entity.SRM;
+using Convience.Model.Models.SRM;
+using Convience.Util.Extension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convience.Service.SRM
+{
+    public interface ISrmInfoRecordService
+    {
+        public SrmInforecord[] Get(QueryInfoRecordModels query);
+    }
+    class SrmInfoRecordService : ISrmInfoRecordService
+    {
+        private readonly SRMContext _context;
+
+        public SrmInfoRecordService(SRMContext context) {
+            _context = context;
+        }
+
+        public SrmInforecord[] Get(QueryInfoRecordModels query)
+        {
+            //using (SRMContext db = new SRMContext())
+            //{
+            var infoRecordQurty = _context.SrmInforecords.AsQueryable()
+                .AndIfHaveValue(query.QotId, r => r.QotId.Equals(query.QotId))
+                .AndIfHaveValue(query.qotIds, r => query.qotIds.Contains(r.QotId.Value));
+            return infoRecordQurty.ToArray();
+            //}
+        }
+    }
+}
