646a305198b3c6d624cc8914e65c252f45e687d5
CFP-TEST01\Administrator <Leon>
2021-10-25 14:55:18 +0800

    ReadExcel

--- src/Convience.Backend/Convience.Applications/Convience.Service/SRM/Utility.cs
+++ src/Convience.Backend/Convience.Applications/Convience.Service/SRM/Utility.cs
@@ -1,6 +1,9 @@
 ﻿using Convience.Filestorage.Abstraction;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -45,6 +48,52 @@ namespace Convience.Service.SRM
                 }
                 return path;
             }
+        }
+
+        public DataTable ReadExcel(string path, int sheetNum)
+        {
+            IWorkbook workbook;
+            using (FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                workbook = new XSSFWorkbook(stream);
+            }
+
+            ISheet sheet = workbook.GetSheetAt(sheetNum); // zero-based index of your target sheet
+            DataTable dt = new DataTable(sheet.SheetName);
+
+            // write header row
+            IRow headerRow = sheet.GetRow(0);
+            for (int i = 0; i < headerRow.Cells.Count; i++)
+            {
+                headerRow.GetCell(i).SetCellType(CellType.String);
+                dt.Columns.Add(headerRow.GetCell(i).StringCellValue);
+            }
+            Dictionary<string, int> dtHeader = new Dictionary<string, int>();
+            foreach (DataColumn col in dt.Columns)
+            {
+                string header = col.ColumnName;
+                dtHeader.Add(header, dt.Columns.IndexOf(header));
+            }
+
+            int rowIndex = 0;
+            foreach (IRow row in sheet)
+            {
+                if (rowIndex == 0) { rowIndex++; continue; }
+                DataFormatter formatter = new DataFormatter();
+                DataRow dataRow = dt.NewRow();
+                foreach (var h in dtHeader)
+                {
+                    if (row.GetCell(h.Value) != null)
+                    {
+                        row.GetCell(h.Value).SetCellType(CellType.String);
+                        dataRow[h.Key] = row.GetCell(h.Value).StringCellValue;
+                    }
+                }
+                //dataRow["IsExists"] = _context.SrmVendors.Any(r => r.SrmVendor1.Equals(dataRow["供應商編號"]) && user.Werks.Contains(r.Ekorg.Value));//2021/10/19問過LEO 同名字不同廠可能存在多筆
+                dt.Rows.Add(dataRow);
+                rowIndex++;
+            }
+            return dt;
         }
     }
 }
