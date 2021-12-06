68e8bcc06e8c49aceaec2989d01d8f307046776d
CFP-TEST02\Administrator <Jenny>
2021-12-06 09:19:02 +0800

    新增數量單位

--- src/Convience.Backend/Convience.Applications/Convience.Entity/Entity/SRM/SRMContext.cs
+++ src/Convience.Backend/Convience.Applications/Convience.Entity/Entity/SRM/SRMContext.cs
@@ -157,6 +157,10 @@ namespace Convience.Entity.Entity.SRM
                 entity.Property(e => e.Width)
                     .HasColumnName("WIDTH")
                     .HasComment("寬");
+
+                entity.Property(e => e.Unit)
+                .HasColumnName("UNIT")
+                .HasComment("數量單位");
             });
 
             modelBuilder.Entity<SrmRfqH>(entity =>
